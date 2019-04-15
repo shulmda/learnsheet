0164f8356c69d8e807e77c4def9f52938a7c3d58
David Shulman <dshulman@envoyworld.com>
2019-04-15 16:39:18 +0200

    Uploaded completely packaged app plus updated build script to create a zip file of the executable

--- pybuild.sh
+++ pybuild.sh
@@ -1,1 +1,6 @@
 pyinstaller --hidden-import tkinter --onefile --windowed ./learnsheet.py
+rm -f learnsheet.zip 
+mkdir learnsheet
+cp -R dist/ learnsheet
+zip learnsheet.zip learnsheet
+rm -rf learnsheet
