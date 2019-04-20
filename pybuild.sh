b6ddec4d11cc258605e1aab65ddb7e616b49cbc0
David Shulman <dshulman@envoyworld.com>
2019-04-20 18:28:21 +0200

    I added an icon for the program because, well, why not?

--- pybuild.sh
+++ pybuild.sh
@@ -3,5 +3,7 @@ pyinstaller --hidden-import wx --onefile --windowed ./learnsheet.py
 rm -f learnsheet.zip 
 mkdir learnsheet
 cp -R dist/ learnsheet
+cp ./Info.plist dist/learnsheet.app/Contents/
+cp learnsheet.icns dist/learnsheet.app/Contents/Resources/icon-windowed.icns
 zip learnsheet.zip learnsheet
 rm -rf learnsheet
